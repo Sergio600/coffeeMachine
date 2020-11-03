2e97e0c195691f2f079d94142694ac377ece4ca7
Sergio600 <sergio600@mail.ru>
2020-11-03 14:21:01 +0300

    1
    1

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 class CoffeeMachine {
-    capacityCoffee;
-    capacityWater;
-    water;
-    coffee;
-    portionAmericano;
-    portionEspresso;
-    portionDoubleEspresso;
-    portionWater;
-    isEnought = true;
+    // capacityCoffee;
+    // capacityWater;
+    // water;
+    // coffee;
+    // portionAmericano;
+    // portionEspresso;
+    // portionDoubleEspresso;
+    // portionWater;
+    // isEnought = true;
 
     constructor(capacityCoffee,
         capacityWater,
@@ -26,42 +26,42 @@ class CoffeeMachine {
     }
 
     makeAmericano() {
-        if (checkComponents(portionAmericano)) {
-            water -= portionWater;
-            coffee -= portionAmericano;
+        if (checkComponents(this.portionAmericano)) {
+            this.water -= this.portionWater;
+            this.coffee -= this.portionAmericano;
         };
-    };
+    }
 
     makeEspresso() {
         if (checkComponents(this.portionEspresso)) {
             this.water -= this.portionWater;
             this.coffee -= this.portionEspresso;
         };
-    };
+    }
 
     makeEspresso() {
         if (checkComponents(this.portionDoubleEspresso)) {
             this.water -= this.portionWater;
             this.coffee -= this.portionDoubleEspresso;
         };
-    };
+    }
 
     checkComponents(portionCoffee) {
         let isEnought = true;
         console.log(this.portionCoffee);
 
-        if (this.water - portionWater < 0) {
+        if (this.water - this.portionWater < 0) {
             alert("Не достаточно воды для приготовления кофе. Необходимо залить воду в бак!");
             isEnought = false;
         }
 
-        if (coffee - portionCoffee < 0) {
+        if (this.coffee - this.portionCoffee < 0) {
             alert("Не достаточно кофе в машине. Необходимо добвить кофе в машину!");
             isEnought = false;
         }
         return isEnought;
 
-    };
+    }
 
     chargeWater() {
         if (this.water == this.capacityWater) {
@@ -71,7 +71,7 @@ class CoffeeMachine {
         let addedWater = this.capacityWater - this.water;
         this.water = this.capacityWater;
         alert("Залито: " + addedWater + "мл. воды. Бак полный.");
-    };
+    }
 
     chargeCoffee() {
         if (this.coffee == this.capacityCoffee) {
@@ -81,22 +81,22 @@ class CoffeeMachine {
         let addedCoffee = this.capacityCoffee - this.coffee;
         this.coffee = this.capacityCoffee;
         alert("Добавлено: " + addedCoffee + "гр. кофе. Контейнер полный.");
-    };
+    }
 
     showWater() {
         return water;
-    };
+    }
 
     showCoffee() {
         return coffee;
-    };
+    }
 }
 
 
 let coffeeMachine = new CoffeeMachine(100, 5000, 5, 7, 14, 200);
 console.log(coffeeMachine);
 
-function makeAmericano() {
+function makeAmericano1() {
     coffeeMachine.makeAmericano();
     alert("Americano is ready!")
 }
